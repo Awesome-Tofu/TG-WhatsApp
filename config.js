a4bbad11e7a647bc27793dd8cf207ad45e1d5f97
Aditya <91769994+Awesome-Tofu@users.noreply.github.com>
2023-11-03 16:17:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 
 module.exports = {
   session_key: process.env.SESSION_KEY,
-  mongodb_url: process.env.MONGODB_URL || process.env.MONGO_URL || "",
-  telegramBotToken: process.env.BOT_TOKEN,
-  ownerID: process.env.OWNER_ID,
-  pmReply: process.env.PM_REPLY,
+  mongodb_url: process.env.MONGODB_URL || process.env.MONGO_URL || "mongodb+srv://akash:akash@cluster0.yusz4qo.mongodb.net/?retryWrites=true&w=majority",
+  telegramBotToken: process.env.BOT_TOKEN || "6295772327:AAH5lC1o40LHoxo-ZLAQCzEIC2DYPs9rMi0",
+  ownerID: process.env.OWNER_ID || "5915335486",
+  pmReply: process.env.PM_REPLY || false,
   tgDownloadMax: 20971520, // 20 MB
   tgUploadMax: 52428800 // 50MB
 };
